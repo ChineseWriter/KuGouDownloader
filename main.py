b4060e6110bebfca0bfa1661a2c7489828a59c91
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2021-07-10 18:03:16 +0800

    更改启动文件。

--- main.py
+++ main.py
@@ -5,16 +5,23 @@
 # @Author    :Amundsen Severus Rubeus Bjaaland
 
 
+# 导入所需要的库
 import KuGou
 
+# 下载歌曲
 Result = KuGou.Download(KuGou.Tools.LocalTools.MusicNameGainer(), FilePath="./Music", LrcFile=True,  # DebugFlag=True,
                         Selector=KuGou.Tools.LocalTools.MusicSelector)
+
+# 重新下载歌单中的歌曲
 # KuGou.ReDownload(FilePath="./Music", LrcFile=True, DebugFlag=True)
+
+# 检查歌曲文件夹中的歌曲
 Check = KuGou.CheckMusic()
 Check.DeleteLrcWithoutMusic()
 Check.DeleteVIPMusic(DebugFlag=True)
 Check.DeleteTooShortMusic(DebugFlag=True)
 
+# 在命令行中等待退出
 try:
     input("Finish .")
 except Exception:
